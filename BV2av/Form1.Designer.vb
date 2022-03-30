ba22ecb4f59825cf2b05fe91c67399b0d5d4db95
雄龙ztz <25386968+xionglongztz@users.noreply.github.com>
2022-03-30 22:37:46 +0800

    Add files via upload

---
+++ BV2av/Form1.Designer.vb
@@ -0,0 +1,87 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form 重写 Dispose，以清理组件列表。
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Windows 窗体设计器所必需的
+    Private components As System.ComponentModel.IContainer
+
+    '注意: 以下过程是 Windows 窗体设计器所必需的
+    '可以使用 Windows 窗体设计器修改它。  
+    '不要使用代码编辑器修改它。
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.SuspendLayout()
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(37, 33)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(142, 25)
+        Me.TextBox1.TabIndex = 0
+        Me.TextBox1.Text = "BV17x411w7KC"
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(178, 32)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(130, 27)
+        Me.Button1.TabIndex = 1
+        Me.Button1.Text = "BV号转av号"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(178, 57)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(130, 27)
+        Me.Button2.TabIndex = 2
+        Me.Button2.Text = "av号转BV号"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'TextBox2
+        '
+        Me.TextBox2.Location = New System.Drawing.Point(37, 57)
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(143, 25)
+        Me.TextBox2.TabIndex = 3
+        Me.TextBox2.Text = "av170001"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 15.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(384, 116)
+        Me.Controls.Add(Me.TextBox2)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.TextBox1)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle
+        Me.MaximizeBox = False
+        Me.Name = "Form1"
+        Me.Text = "BV2av"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Button2 As Button
+    Friend WithEvents TextBox2 As TextBox
+End Class
---
+++ BV2av/Form1.vb
@@ -0,0 +1,33 @@
+﻿Public Class Form1
+    Const xorNum = 177451812
+    Const addNum = 8728348608
+    Const table = "fZodR9XQDSUm21yCkr6zBqiveYah8bt4xsWpHnJE7jL5VG3guMTKNPAwcF"
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim tr() As String
+        ReDim tr(0 To Len(table))
+        For i = 0 To Len(table)
+            tr(i) = Mid(table, i + 1, 1)
+        Next '将字符放入数组
+        Dim r As Double
+        Dim S() As Integer = {11, 10, 3, 8, 4, 6}
+        Dim x() As String '定义BV号
+        ReDim x(0 To TextBox1.TextLength)
+        For i = 0 To TextBox1.TextLength
+            x(i) = Mid(TextBox1.Text, i + 1, 1)
+        Next
+        For i = 0 To 5
+            r = r + (InStrRev(table, x(S(i))) - 1) * (58 ^ i)
+        Next
+        TextBox2.Text = "av" & ((r - addNum) Xor xorNum)
+    End Sub
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        Dim x As Double '定义av号
+        Dim S() As Integer = {5, 4, 0, 3, 1, 2} '对应位置
+        x = (Val(Mid(TextBox2.Text, 3)) Xor xorNum) + addNum
+        Dim r() As String = {"", "", "", "", "", ""}
+        For i = 0 To 5
+            r(S(i)) = table(Math.Floor(x / (58 ^ i)) Mod 58) '答题卡
+        Next
+        TextBox1.Text = "BV1" & r(0) & r(1) & "4" & r(2) & "1" & r(3) & "7" & r(4) & r(5) '完形填空
+    End Sub
+End Class
